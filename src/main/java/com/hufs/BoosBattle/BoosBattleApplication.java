7d34a0490ce639865090be72afe3d87111ee9779
kimdev <kim__dev@hufs.ac.kr>
2024-09-13 20:32:28 +0900

    init. first commit

---
+++ src/main/java/com/hufs/BoosBattle/BoosBattleApplication.java
@@ -0,0 +1,13 @@
+package com.hufs.BoosBattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoosBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoosBattleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hufs/BoosBattle/BoosBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hufs.BoosBattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoosBattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
